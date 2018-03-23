345f3828a26a8b5bc842d5c8517ad36ffa70bf12
Romain ODDONE <romain.oddone@outlook.com>
2018-03-23 15:29:30 +0100

    add command line test program

---
+++ BraviaIRCCControl.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraviaIRCCControl.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Host name ? ");
+            string hostname = System.Console.ReadLine();
+            IRCCController control = new IRCCController(hostname);
+
+            Task.Factory.StartNew(async () =>
+            {
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeUp);
+
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+                await Task.Delay(1000);
+                await control.Send(IRCCCodes.VolumeDown);
+            });
+
+            System.Console.WriteLine("Press enter to quit.");
+            System.Console.ReadLine();
+        }
+    }
+}
