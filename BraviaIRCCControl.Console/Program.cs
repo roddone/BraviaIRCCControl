50c52a3adccab21dbb9d55df6399012d51dfbd20
romain oddone <romain.oddone@outlook.com>
2018-03-24 11:45:12 +0100

    code cleanup and add test cases in console program

--- BraviaIRCCControl.Console/Program.cs
+++ BraviaIRCCControl.Console/Program.cs
@@ -1,56 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using BraviaIRCCControl.Extensions;
 using System.Threading.Tasks;
 
 namespace BraviaIRCCControl.Console
 {
     public class Program
     {
-        static void Main(string[] args)
+        #region Private Methods
+
+        private static void Main(string[] args)
         {
             System.Console.Write("Host name ? ");
             string hostname = System.Console.ReadLine();
+
             IRCCController control = new IRCCController(hostname);
 
             Task.Factory.StartNew(async () =>
             {
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeUp);
+                await control.VolumeUp();
 
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
-                await Task.Delay(1000);
                 await control.Send(IRCCCodes.VolumeDown);
+                await control.VolumeDown();
+
+                await control.Send(IRCCCodes.HDMI1);
+                await control.Send(IRCCCodes.HDMI2);
+                await control.Send(IRCCCodes.HDMI3);
+                await control.Send(IRCCCodes.HDMI4);
             });
 
             System.Console.WriteLine("Press enter to quit.");
             System.Console.ReadLine();
         }
+
+        #endregion Private Methods
     }
 }
